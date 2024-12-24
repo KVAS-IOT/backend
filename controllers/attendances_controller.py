16e8207384eda1d7b5e665c21afe09a09dea82b1
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-24 14:32:29 +0100

    Created endpoints for: 1) getting all labs 2) getting lectures for lab 3) getting attendances by lab and lecture

---
+++ controllers/attendances_controller.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+
+attendances_router = APIRouter()
+
+@attendances_router.get("/")
+async def get_attendances(lab_id: int, lecture_time: str, date: str):
+    return {
+        "attendances": [
+            {
+                "card_id": "36145173625782788",
+                "time": "24-12-2024 7:25:02"
+            },
+            {
+                "card_id": "52389740174017432",
+                "time": "24-12-2024 7:26:02"
+            },
+            {
+                "card_id": "48293017564829173",
+                "time": "24-12-2024 7:27:02"
+            },
+            {
+                "card_id": "38592017463829175",
+                "time": "24-12-2024 7:28:02"
+            },
+            {
+                "card_id": "72938461502748391",
+                "time": "24-12-2024 7:29:02"
+            },
+            {
+                "card_id": "59381720475638291",
+                "time": "24-12-2024 7:30:02"
+            },
+        ],
+        "error_code": 100,
+        "error_message": None
+    }
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 
+from controllers.labs_controller import labs_router
+from controllers.attendances_controller import attendances_router
+
 app = FastAPI()
+
+app.include_router(labs_router, prefix="/labs")
+app.include_router(attendances_router, prefix="/attendances")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
