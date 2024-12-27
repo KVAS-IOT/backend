8af102f5da5ab943e3b4aa4ac051fac3018cd013
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-27 20:40:12 +0100

    Created lab repository and implemented getting all labs.

---
+++ repositories/LabRepository.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+
+from database.database_config import async_session_factory
+from models.LabsModel import LabsModel
+
+class LabRepository:
+    @staticmethod
+    async def get_all_labs() -> list[LabsModel]:
+        async with async_session_factory() as session:
+            async with session.begin():
+                query = (
+                    select(LabsModel)
+                )
+
+                res = await session.execute(query)
+                lab_models = res.scalars().all()
+                print(lab_models)
+                return lab_models
