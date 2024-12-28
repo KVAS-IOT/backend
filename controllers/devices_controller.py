ae9f69fa70504d8b080063de8c275cdeadd1887b
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-28 14:03:12 +0100

    Updated path param for get_device_gateway endpoint

--- controllers/devices_controller.py
+++ controllers/devices_controller.py
@@ -5,6 +5,6 @@ from services.AttendanceScannerService import AttendanceScannerService
 
 devices_router = APIRouter()
 
-@devices_router.get("/{device_id}/lab")
+@devices_router.get("/{scanner_id}/lab")
 async def get_device_gateway(scanner_id: str) -> GatewayGetDTO:
     return await AttendanceScannerService.get_scanner_gateway(scanner_id)
