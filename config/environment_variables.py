60799e38f7c19cb36d89ff5ecda8b48bb3a07e31
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-28 15:59:43 +0100

    Added mqtt env variables to the project.

--- config/environment_variables.py
+++ config/environment_variables.py
@@ -9,7 +9,14 @@ def get_env_variable(variable_name: str):
     return os.getenv(variable_name)
 
 
+# Database
 DB_HOST = get_env_variable("DB_HOST")
 DB_USERNAME = get_env_variable("DB_USERNAME")
 DB_PASSWORD = get_env_variable("DB_PASSWORD")
 DB_NAME = get_env_variable("DB_NAME")
+
+# MQTT
+MQTT_ADDRESS = get_env_variable("MQTT_ADDRESS")
+MQTT_PORT = int(get_env_variable("MQTT_PORT"))
+MQTT_USERNAME = get_env_variable("MQTT_USERNAME")
+MQTT_PASSWORD = get_env_variable("MQTT_PASSWORD")
