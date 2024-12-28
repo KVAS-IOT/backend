c9bae4834ee05441413eb34d716857dcb8227c55
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-28 19:27:20 +0100

    Created endpoint for creating and updating attendance scanners.

--- controllers/devices_controller.py
+++ controllers/devices_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from dtos.AttendanceScannerDTOs import AttendanceScannerAddDTO
 from dtos.GatewayDTOs import GatewayGetDTO
 from services.AttendanceScannerService import AttendanceScannerService
 
@@ -8,3 +9,14 @@ devices_router = APIRouter()
 @devices_router.get("/{scanner_id}/lab")
 async def get_device_gateway(scanner_id: str) -> GatewayGetDTO:
     return await AttendanceScannerService.get_scanner_gateway(scanner_id)
+
+
+@devices_router.post("")
+async def create_device(device: AttendanceScannerAddDTO) -> None:
+    # TODO: Implement
+    pass
+
+@devices_router.put("/{scanner_id}/lab")
+async def update_device_lab(scanner_id: str, lab_id: int) -> None:
+    # TODO: Implement
+    pass
