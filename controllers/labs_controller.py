60c8b5e0a96a7c326287b970cebc8b75c7bd127e
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2024-12-28 19:14:51 +0100

    Created endpoint for updating lab's lectures.

--- controllers/labs_controller.py
+++ controllers/labs_controller.py
@@ -17,5 +17,11 @@ async def get_lab(lab_id: int) -> list[LectureTimes]:
     return await LabService.get_lab_lecture_times_by_lab_id(lab_id)
 
 @labs_router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_lab(lab: LabAddDTO):
+async def create_lab(lab: LabAddDTO) -> None:
+    # TODO: Implement
+    pass
+
+@labs_router.put("/{lab_id}/lectures", status_code=status.HTTP_202_ACCEPTED)
+async def update_lab_lectures(lab_id: int, lecture_times: list[LectureTimes]) -> None:
+    # TODO: Implement
     pass
