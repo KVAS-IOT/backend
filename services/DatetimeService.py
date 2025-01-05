5691abd9e4f372d6d8230066eae00c1e884fffe2
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2025-01-05 18:22:20 +0100

    Created method for converting time string to datetime.time object.

--- services/DatetimeService.py
+++ services/DatetimeService.py
@@ -5,3 +5,7 @@ class DatetimeService:
     @staticmethod
     def convert_date_string_to_datetime(date_str: str) -> datetime:
         return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+    @staticmethod
+    def convert_time_string_to_time_object(time_str: str) -> datetime.time:
+        return datetime.strptime(time_str, "%H:%M").time()
