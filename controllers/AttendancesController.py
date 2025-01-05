68f7a8c2677f44c0ad3dbb6ca39f6b15a0d987a4
Vladyslav Hrieshnykh <vladyslav.hrieshnykh@student.tuke.sk>
2025-01-05 18:22:56 +0100

    Implemented service method for getting attendances by lab and date.

--- controllers/AttendancesController.py
+++ controllers/AttendancesController.py
@@ -3,39 +3,10 @@ import datetime
 from fastapi import APIRouter
 
 from dtos.AttendanceDTOs import AttendancesByLabAndDateGetDTO
+from services.AttendanceService import AttendanceService
 
 attendances_router = APIRouter()
 
 @attendances_router.get("/")
 async def get_attendances(lab_id: int, date: datetime.date) -> AttendancesByLabAndDateGetDTO:
-    print(date)
-    return {
-        "attendances": [
-            {
-                "card_id": "36145173625782788",
-                "time": "24-12-2024 7:25:02"
-            },
-            {
-                "card_id": "52389740174017432",
-                "time": "24-12-2024 7:26:02"
-            },
-            {
-                "card_id": "48293017564829173",
-                "time": "24-12-2024 7:27:02"
-            },
-            {
-                "card_id": "38592017463829175",
-                "time": "24-12-2024 7:28:02"
-            },
-            {
-                "card_id": "72938461502748391",
-                "time": "24-12-2024 7:29:02"
-            },
-            {
-                "card_id": "59381720475638291",
-                "time": "24-12-2024 7:30:02"
-            },
-        ],
-        "error_code": 100,
-        "error_message": None
-    }
+    return await AttendanceService.get_attendances_by_lab_and_date(lab_id, date)
--- services/AttendanceService.py
+++ services/AttendanceService.py
@@ -1,5 +1,13 @@
+import datetime
+from typing import Optional
+
+from asyncpg.pgproto.pgproto import timedelta
+
+from dtos.AttendanceDTOs import AttendancesByLabAndDateGetDTO, AttendanceGetDTO, LectureAttendancesGetDTO
 from models.AttendancesModel import AttendancesModel
+from models.LabsModel import LectureTimes
 from repositories.AttendanceRepository import AttendanceRepository
+from repositories.LabRepository import LabRepository
 from services.DatetimeService import DatetimeService
 
 
@@ -13,3 +21,39 @@ class AttendanceService:
                 attendances_to_save.append(attendance_to_save)
 
         await AttendanceRepository.save_attendances(attendances_to_save)
+
+    @staticmethod
+    def get_attendance_lecture_time(attendance_time: datetime.datetime, lab_lectures: list[LectureTimes]) -> Optional[LectureTimes]:
+        for lecture in lab_lectures:
+            if lecture.get_attendance_start_time() <= attendance_time.time() <= lecture.get_end_time():
+                return lecture
+        return None
+
+    @staticmethod
+    async def get_attendances_by_lab_and_date(lab_id: int, date: datetime.date) -> AttendancesByLabAndDateGetDTO:
+        attendances = await AttendanceRepository.get_attendances_by_lab_and_date(lab_id, date)
+        attendance_get_dtos = [AttendanceGetDTO(card_id=attendance.card_id, attendance_time=attendance.date) for attendance in attendances]
+
+        lab_lectures = await LabRepository.get_lab_lecture_times_by_lab_id(lab_id)
+        sorted_lab_lectures = sorted(lab_lectures, key=lambda x: x.get_attendance_start_time())
+
+
+        lecture_attendances_dict: dict[LectureTimes, list[AttendanceGetDTO]] = {}
+        out_of_lecture_attendances: list[AttendanceGetDTO] = []
+
+        for attendance in attendance_get_dtos:
+            lecture_time = AttendanceService.get_attendance_lecture_time(attendance.attendance_time, sorted_lab_lectures)
+            if lecture_time:
+                if lecture_time not in lecture_attendances_dict:
+                    lecture_attendances_dict[lecture_time] = []
+                lecture_attendances_dict[lecture_time].append(attendance)
+            else:
+                out_of_lecture_attendances.append(attendance)
+
+        lecture_attendances = []
+
+        for lecture, attendances in lecture_attendances_dict.items():
+            lecture_attendances.append(LectureAttendancesGetDTO(lecture_time=lecture, attendances=attendances))
+
+        return AttendancesByLabAndDateGetDTO(lab_id=lab_id, date=date, lectures_attendances=lecture_attendances, out_of_lectures_attendances=out_of_lecture_attendances)
+
